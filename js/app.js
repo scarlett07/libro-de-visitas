f29667026a6a36295b93d7abc1bf95aa56ffa744
scarlett07 <scarlett.geo@gmail.com>
2018-02-14 11:56:50 -0600

    empezando funcionalidad

--- js/app.js
+++ js/app.js
@@ -2,3 +2,18 @@
 
 $('#textarea1').val('New Text');
 $('#textarea1').trigger('autoresize');
+
+
+// Publicar texto
+
+$(document).ready(function() {
+  $('#enviar').click(getComent);
+});
+
+function getComent(event){
+  event.preventDefault();
+  var cometario = $('#icon_prefix2').val()
+
+  console.log(cometario);
+
+}
