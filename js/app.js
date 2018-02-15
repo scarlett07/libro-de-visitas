67c1e200d115d4aafc98bbb84abe2db3c0ed418e
scarlett07 <scarlett.geo@gmail.com>
2018-02-15 08:40:54 -0600

    input

--- js/app.js
+++ js/app.js
@@ -1,28 +1,10 @@
-
-// Publicar texto
-var come = $('#icon_prefix2').val();
-
-
-
 $(document).ready(function() {
-  $('#icon_prefix2').keyup(comentEdit(come));
-  $('#icon_prefix2').keyup(getComent());
+    $('#icon_prefix2').keyup(visualizarPost);
 
 });
 
-function getComent(event){
-//  event.preventDefault();
-//  console.log(comentario);
-  //
-  // if (comentario.length>0){
-  //   $('#icon_prifix2').keyup(comentEdit(comentario));
-  //
-  // }
-
-}
-
-function comentEdit(comentario){
-  var comentEtitado = $('#coment-edit');
-  comentEtitado.html(comentario);
-
+function visualizarPost(event) {
+    var comentario = $(this).val();
+    var comentEtitado = $('#coment-edit');
+    comentEtitado.html(comentario);
 }
